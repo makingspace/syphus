0c78bf820f636dc97cb0608b3240c8d7ac7e0179
Zach Smith <zd@zdsmith.com>
2017-09-05 17:59:57 -0400

    Remove numpy dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 setup(
     name='syphus',
-    version='1.3',
+    version='1.3.1',
     description='MakeSpace Hill-Climbing Algorithm',
     url='https://github.com/makingspace/syphus',
     author='Zach Smith',
@@ -26,5 +26,4 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
     packages=['syphus'],
-    install_requires=['NumPy', ],
     extras_require={'test': ['hypothesis'], })
